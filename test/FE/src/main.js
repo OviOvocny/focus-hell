07beb988e6e3b8bc7133513e3c9248b8e39cd3a3
OviOvocny <werint.pp@gmail.com>
2019-12-10 13:34:08 +0100

    come on

--- test/FE/src/main.js
+++ test/FE/src/main.js
@@ -8,7 +8,7 @@ import Logo from '@/components/Logo'
 import Avatar from '@/components/Avatar'
 import Button from '@/components/controls/Button'
 
-Vue.config.productionTip = true
+Vue.config.productionTip = true // test comment. lol
 
 Vue.component('logo', Logo)
 Vue.component('avatar', Avatar)
