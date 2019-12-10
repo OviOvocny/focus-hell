fbd0bd12dad989329aa3e6355cd340e58e2c1ab6
Webpacker <adam.horak@ovi.moe>
2019-12-10 13:07:00 +0000

    Build webadmin frontend 🚀

---
+++ test/FE/dist/precache-manifest.c0ec2837d6a69690dd2184eb7c402c89.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1913106bf7b3791c55f5",
+    "url": "/css/app.aba50c1f.css"
+  },
+  {
+    "revision": "1913106bf7b3791c55f5",
+    "url": "/css/app.aba50c1f.css.map"
+  },
+  {
+    "revision": "65846caa5f779894e624",
+    "url": "/css/chunk-249787b8.ef8c7de8.css"
+  },
+  {
+    "revision": "65846caa5f779894e624",
+    "url": "/css/chunk-249787b8.ef8c7de8.css.map"
+  },
+  {
+    "revision": "7ad053d6d95ada4c46c0",
+    "url": "/css/chunk-3b7e35ae.47f5f2e7.css"
+  },
+  {
+    "revision": "7ad053d6d95ada4c46c0",
+    "url": "/css/chunk-3b7e35ae.47f5f2e7.css.map"
+  },
+  {
+    "revision": "2fbec9ed33fdfaae7977d61c91734ea7",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "58b6d8e61c94b2e3e04411f678190bf0",
+    "url": "/img/community.58b6d8e6.svg"
+  },
+  {
+    "revision": "fa20239cc4ada5c2a8f5b1751aa3fcaa",
+    "url": "/img/create.fa20239c.svg"
+  },
+  {
+    "revision": "1323683a4fc501bf30b2e3e1678e2fd9",
+    "url": "/img/fh-full.svg"
+  },
+  {
+    "revision": "10d24a89081527db737533e4495fb60e",
+    "url": "/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "eeeeb1f0929bf282ab60ea39eff84e49",
+    "url": "/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "acc540338f43ebe8f5ff6d5438defc9b",
+    "url": "/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "66c4b79facdb0ff3c7d5d0b7f119eb19",
+    "url": "/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "8f01856e640eee0d340ecab5def8b66d",
+    "url": "/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "5270a9e2fd538f8a54318793a2d8c07e",
+    "url": "/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "3e51ab89e1dd4b3097a0e86d9246d339",
+    "url": "/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "8f01856e640eee0d340ecab5def8b66d",
+    "url": "/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "47567df77cb0c34606bf4c855cba68cb",
+    "url": "/img/icons/browserconfig.xml"
+  },
+  {
+    "revision": "4496825f2fa1cf8a23bb7f4107b519d3",
+    "url": "/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "c014516c6d56ca3f19b8f13d8fea63aa",
+    "url": "/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "a7d13e692b5b0fbfc6055244ef5a091a",
+    "url": "/img/icons/mstile-144x144.png"
+  },
+  {
+    "revision": "d8080b175572fcf5692baf8d6f4f99d9",
+    "url": "/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "675d33a96b3136be07fedccfe680b769",
+    "url": "/img/icons/mstile-310x150.png"
+  },
+  {
+    "revision": "a1d87e973053225c4383530693482996",
+    "url": "/img/icons/mstile-310x310.png"
+  },
+  {
+    "revision": "040517bddd66bcdda2b545b81c0be8fe",
+    "url": "/img/icons/mstile-70x70.png"
+  },
+  {
+    "revision": "5d3e27df12a8bdb5a526c6dfe9af7970",
+    "url": "/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "8c607bc4c13ea2268f128bde1225a9a7",
+    "url": "/img/icons/site.webmanifest"
+  },
+  {
+    "revision": "c6fd783fd84c8090e68ce0e18a457124",
+    "url": "/img/play.c6fd783f.svg"
+  },
+  {
+    "revision": "5b629e46ae469c4c16053011fd40f887",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1913106bf7b3791c55f5",
+    "url": "/js/app.6c964076.js"
+  },
+  {
+    "revision": "1913106bf7b3791c55f5",
+    "url": "/js/app.6c964076.js.map"
+  },
+  {
+    "revision": "65846caa5f779894e624",
+    "url": "/js/chunk-249787b8.04099493.js"
+  },
+  {
+    "revision": "65846caa5f779894e624",
+    "url": "/js/chunk-249787b8.04099493.js.map"
+  },
+  {
+    "revision": "6b8889961d14450e7943",
+    "url": "/js/chunk-2d22d746.8c54f9f2.js"
+  },
+  {
+    "revision": "6b8889961d14450e7943",
+    "url": "/js/chunk-2d22d746.8c54f9f2.js.map"
+  },
+  {
+    "revision": "bee322300691ba7cb890",
+    "url": "/js/chunk-2f0bf28b.ff5d9cbc.js"
+  },
+  {
+    "revision": "bee322300691ba7cb890",
+    "url": "/js/chunk-2f0bf28b.ff5d9cbc.js.map"
+  },
+  {
+    "revision": "7ad053d6d95ada4c46c0",
+    "url": "/js/chunk-3b7e35ae.c3d6722f.js"
+  },
+  {
+    "revision": "7ad053d6d95ada4c46c0",
+    "url": "/js/chunk-3b7e35ae.c3d6722f.js.map"
+  },
+  {
+    "revision": "989ce4f91c01aaeb7024",
+    "url": "/js/chunk-b136200c.5063128e.js"
+  },
+  {
+    "revision": "989ce4f91c01aaeb7024",
+    "url": "/js/chunk-b136200c.5063128e.js.map"
+  },
+  {
+    "revision": "d99651d469114901d623",
+    "url": "/js/chunk-vendors.96c645f0.js"
+  },
+  {
+    "revision": "d99651d469114901d623",
+    "url": "/js/chunk-vendors.96c645f0.js.map"
+  },
+  {
+    "revision": "a07d4a180a656bf84df522c73562e703",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "5528f4a1e7b73078a944dd1eef48e2d5",
+    "url": "/sound/complete.mp3"
+  },
+  {
+    "revision": "9e43edd9f4b3d8916fbbce66e94ff285",
+    "url": "/sound/confirm.mp3"
+  },
+  {
+    "revision": "7808170b52d6ccfc5044c7742d347461",
+    "url": "/sound/error.mp3"
+  },
+  {
+    "revision": "d013605da929329c8d3d4fdb647414d9",
+    "url": "/sound/negative.mp3"
+  },
+  {
+    "revision": "3b575972fc02ae4501f635cd7d5d79ec",
+    "url": "/sound/positive.mp3"
+  }
+]);
--- test/FE/src/main.js
+++ test/FE/src/main.js
@@ -8,7 +8,7 @@ import Logo from '@/components/Logo'
 import Avatar from '@/components/Avatar'
 import Button from '@/components/controls/Button'
 
-Vue.config.productionTip =w falseqwd
+Vue.config.productionTip = false
 
 Vue.component('logo', Logo)
 Vue.component('avatar', Avatar)
